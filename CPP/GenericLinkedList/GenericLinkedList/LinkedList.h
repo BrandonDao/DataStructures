2d159e064a3bc5c046d1591ffc6200d1b08d4eba
Brandon Dao <brandondao27@gmail.com>
2022-09-26 19:05:06 -0700

    Currently making the generic linked list use std::shared_ptr

--- CPP/GenericLinkedList/GenericLinkedList/LinkedList.h
+++ CPP/GenericLinkedList/GenericLinkedList/LinkedList.h
@@ -1,29 +1,38 @@
 #pragma once
 #include <stdexcept>
+#include <memory>
+
+// std::shared_ptr
+// make_shared
+
 
 template <typename T>
 struct Node {
+
+	using Node_ptr = std::shared_ptr<Node<T>>;
+
 	T value{};
-	Node* next{};
+	Node_ptr next{};
+	Node<T>* next{};
 
 	Node() { }
 	Node(T value) {
-		Node::value = value;
+		Node<T>::value = value;
 	}
 };
 
 template <typename T>
 struct LinkedList {
 	int count{};
-	Node<T>* head{};
-	Node<T>* tail{};
+	std::shared_ptr<Node<T>> head{};
+	std::shared_ptr<Node<T>> tail{};
 
-	LinkedList<T>() {}
-	LinkedList<T>(const LinkedList& copyMe) {
+	LinkedList() {}
+	LinkedList(const LinkedList<T>& copyMe) {
 		copy(copyMe);
 	}
 	~LinkedList() {
-		Node<T>* curr = head;
+		auto curr = head;
 
 		while (curr->next != nullptr) {
 			Node<T>* temp = curr->next;
@@ -33,24 +42,25 @@ struct LinkedList {
 		delete curr;
 	}
 
-	LinkedList& operator =(const LinkedList& copyMe) {
+	LinkedList<T>& operator =(const LinkedList<T>& copyMe) {
 		copy(copyMe);
 	}
 
 	void add(T value) {
-		auto newNode = new Node<T>{ value };
+		auto newNode = std::make_shared<Node<T>>(value); //new Node<T>{ value };
 
 		if (head == nullptr) {
-			head = newNode;
-			tail = newNode;
+			head = std::move(newNode);
+			tail = head;
 			count++;
 			return;
 		}
 
-		tail->next = newNode;
-		tail = newNode;
+		tail->next = std::move(newNode);
+		tail = tail->next;
 		count++;
 	}
+
 	bool remove(T value) {
 		if (head->value == value) {
 			head = head->next;
@@ -79,7 +89,7 @@ struct LinkedList {
 	}
 
 private:
-	void copy(const LinkedList& copyMe) {
+	void copy(const LinkedList<T>& copyMe) {
 		for (auto curr = copyMe.head; curr != nullptr; curr = curr->next) {
 			add(curr->value);
 		}
