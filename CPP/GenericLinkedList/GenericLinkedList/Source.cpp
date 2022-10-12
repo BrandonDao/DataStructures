bfed2e098ffc2ae5ca62632c67b0b9fc236ba5d2
Brandon Dao <brandondao27@gmail.com>
2022-10-11 20:59:10 -0700

    Added a bubble sort for std::vector, and now working on making it work for any iterator.  Having problems with swap only happening locally in the swap function

--- CPP/GenericLinkedList/GenericLinkedList/Source.cpp
+++ CPP/GenericLinkedList/GenericLinkedList/Source.cpp
@@ -2,27 +2,75 @@
 #include <iostream>
 #include <vector>
 
-template<typename T>
-void notStdSort() {
+template <typename T>
+void swap1(std::_Vector_iterator<T> a, std::_Vector_iterator<T> b) {
+	auto temp = *a;
+	*a = *b;
+	*b = temp;
+}
 
+//template <typename iterator>
+//void swap2(iterator a, iterator b) {
+//	auto temp = *a;
+//	*a = *b;
+//	*b = temp;
+//}
+
+template <typename T>
+void infiniteLoop(std::_Vector_iterator<T> begin, std::_Vector_iterator<T> end) {
+	for(auto a = begin; a != end; a++) {
+		for (auto b = begin; b != end; b++) {
+			if (*a > *b) {
+				std::swap(a, b);
+			}
+		}
+	}
+}
+
+template <typename T>
+void bestSort(std::_Vector_iterator<T> begin, std::_Vector_iterator<T> end) {
+	for (auto a = begin; a != end; a++) {
+		for (auto b = a + 1; b != end; b++) {
+			if (*a > *b) {
+				swap1(a, b);
+			}
+		}
+	}
+}
+
+template <typename iterator>
+void attempt3(iterator begin, iterator end) {
+
+	for (auto a = begin; a != end; a++) {
+		for (auto b = a; b != end; b++) {
+			if (*a > *b) {
+				//swap2(a, b);
+			}
+		}
+	}
 }
 
 int main() {
 		LinkedList<int> list{};
 
+
 		std::vector<int> vcat{};
-		vcat.emplace_back(1);
 		vcat.emplace_back(5);
+		vcat.emplace_back(1);
+		vcat.emplace_back(4);
 		vcat.emplace_back(2);
 		vcat.emplace_back(3);
-		vcat.emplace_back(4);
 
-		//list.add(1);
-		//list.add(3);
-		//list.add(2);
-		//list.add(10);
-		//list.add(-1);
-		//list.add(100);
+		bestSort(vcat.begin(), vcat.end());
+
+		list.add(1);
+		list.add(3);
+		list.add(2);
+		list.add(10);
+		list.add(-1);
+		list.add(100);
+
+		//attempt3(list.begin(), list.end());
 
 		// For memory leak testing
 		//auto betterList = list;
